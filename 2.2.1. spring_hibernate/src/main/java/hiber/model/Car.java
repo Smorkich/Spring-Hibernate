b76dcd9d1e73adfb47c4fa96d34d6695492ae898
руслан каракян <ruslan.karakyan@yandex.ru>
2023-11-28 17:19:19 +0300

    поправил аннотации в car

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -15,7 +15,7 @@ public class Car {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "car_id")
     private Long id;
-        @Column(name = "model")
+    @Column(name = "model")
     private String model;
     @Column(name = "series")
     private int series;
